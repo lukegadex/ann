a5ce094f08867c77aca72834db75542413317ec1
jasminejack12345 <147965025+jasminejack12345@users.noreply.github.com>
2023-11-08 04:57:11 +1000

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,35 @@
+import math  
+
+class Circle: 
+    def __init__(self,radius):   
+        self.radius = radius 
+        
+
+    def calc_area(self): 
+        return (self.radius*self.radius)*math.pi  
+    
+    def circumference(self):  
+        return(2 * self.radius)*math.pi    
+    
+    
+
+
+users_choice = int(input("Enter 1 to calculate AREA\nEnter 2 to calculate CIRCUMFERENCE\nEnter here "))
+if users_choice == 1:
+    r = int(input("Enter radius in cm: ")) 
+    
+    area_Obj = Circle(r) 
+    print("Area of Circle is: ", area_Obj.calc_area(), " cm squared") 
+
+elif users_choice == 2:
+    r = int(input("Enter radius in cm: ")) 
+    
+    Circumference_Obj = Circle(r) 
+    print("Circumference of Circle is: ", Circumference_Obj.circumference(), " cm") #----------2
+
+else:
+    print("Option not available")
+
+
+
+
